005fb5a4f0963f0a6b17a2c58460cb0bbff3ce6f
Opalo1189 <enriquemaestre11@gmail.com>
2023-08-21 18:37:01 -0500

    Instalacion de bases de las vistas

--- Movie-Angular/src/app/app.module.ts
+++ Movie-Angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeMovieComponent } from './components/home-movie/home-movie.component';
+import { DetailsMovieComponent } from './components/details-movie/details-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeMovieComponent,
+    DetailsMovieComponent
   ],
   imports: [
     BrowserModule,
---
+++ Movie-Angular/src/app/components/home-movie/home-movie.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeMovieComponent } from './home-movie.component';
+
+describe('HomeMovieComponent', () => {
+  let component: HomeMovieComponent;
+  let fixture: ComponentFixture<HomeMovieComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HomeMovieComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HomeMovieComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
