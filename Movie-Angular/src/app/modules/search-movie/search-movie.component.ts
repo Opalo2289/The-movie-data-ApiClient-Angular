69883e8860ce1d381b86ff7747072aca42f5d3e1
Opalo1189 <enriquemaestre11@gmail.com>
2023-08-28 11:54:43 -0500

    se logra, filtro y paginacion

--- Movie-Angular/src/app/modules/search-movie/search-movie.component.ts
+++ Movie-Angular/src/app/modules/search-movie/search-movie.component.ts
@@ -21,53 +21,67 @@ import { PageEvent } from '@angular/material/paginator';
 })
 export class SearchMovieComponent implements OnInit {
 
-
   movies$!: Observable<ApiResponse<Movies>>;
   private searchTerms = new Subject<string>();
-  public page: number = 1
+  public page: number = 1;
+  private term: string = ""
   length = 10000;
   hidePageSize = false;
   showPageSizeOptions = false;
   showFirstLastButtons = true;
   disabled = false;
+  searched = false;
+
+
 
   constructor(private _movieService: MovieService) { };
 
   ngOnInit(): void {
 
     this.getMovies(this.page)
-    this.setupSearchObservable();
+    this.setupSearchAndPaginationObservable();
   };
 
-  setupSearchObservable(): void {
-    this.movies$ = this.searchTerms.pipe(
-      startWith(''), // Mostrar películas desde el principio
-      debounceTime(300),
-      distinctUntilChanged(),
-      switchMap((term: string) => this._movieService.searchMovies(term, this.page))
-    );
-  }
 
   getMovies(page: number): void {
     this.movies$ = this._movieService.getMovies(page)
-  }
+  };
 
-  onPageChange(e: PageEvent): void {
-    console.log("page", e)
-    this.page = e.pageIndex + 1;
-    
-    this.movies$.pipe(isEmpty()).subscribe(isEmpty => {
-      if (isEmpty) {
-        // El observable searchTerms está vacío
-        console.log('searchTerms está vacío');
-        this.getMovies(this.page);
-      }
-    });
-    
-  }
 
   search(term: string): void {
+    console.log('search', term)
     this.searchTerms.next(term);
+  }
+
+
+  setupSearchAndPaginationObservable(): void {
+    this.movies$ = this.searchTerms.pipe(
+      startWith(''), // Mostrar películas desde el principio
+      debounceTime(300),
+      switchMap((term: string) => {
+        // Reiniciar la página al realizar una nueva búsqueda
+        this.term = term; // Actualizar el término de búsqueda
+        if (term === '') {
+          if(this.searched){
+            this.page = 1;
+            this.searched = false;
+          }
+            return this._movieService.getMovies(this.page);
+          } else {
+            if(!this.searched){
+              this.page = 1;
+            }
+            this.searched = true;
+            return this._movieService.searchMovies(term, this.page);
+          }
+        })    
+      );
   };
+
+
+  onPageChange(e: PageEvent): void {
+    this.page = e.pageIndex + 1;
+    this.searchTerms.next(this.term); // Usa el término de búsqueda actual
+  }
 
 };
